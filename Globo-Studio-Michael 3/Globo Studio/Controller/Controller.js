147ba01c5e3895199442df5a0bf5620e15193bfc
RmwormMichael <rmworm18@gmail.com>
2024-10-11 09:58:56 -0500

    Update Controller.js

--- Globo-Studio-Michael 3/Globo Studio/Controller/Controller.js
+++ Globo-Studio-Michael 3/Globo Studio/Controller/Controller.js
@@ -1,20 +1,50 @@
-let vista = new Vista();
+let vista = null;
 const mainContenido = document.getElementById("mainContenido");
 
 window.onload = () => {
-    document
+    vista = new Vista();
+    mostrarMainContenido()
+    /*document
     .getElementById("abrir")
     .addEventListener("click", mostrarMenu)
-
     document
     .getElementById("b-about")
     .addEventListener("click", mostrarAbout)
+*/
 };
 
 function mostrarMenu() {
     vista.mostrarPlantilla("menu", "mainContenido")
 }
 
+function mostrarMainContenido() {
+    vista.mostrarPlantilla("inicio", "mainContenido")
+}
+
 function mostrarAbout() {
     vista.mostrarPlantilla("about", "mainContenido")
+}
+
+function mostrarServices() {
+    vista.mostrarPlantilla("our-services", "mainContenido")
+}
+
+function mostrarGallery() {
+    vista.mostrarPlantilla("galery", "mainContenido")
+}
+
+function mostrarQuotePrice() {
+    vista.mostrarPlantilla("quote-price", "mainContenido")
+}
+
+function mostrarLogin() {
+    vista.mostrarPlantilla("login", "mainContenido")
+}
+
+function mostrarRecovery() {
+    vista.mostrarPlantilla("recovery", "mainContenido")
+}
+
+function mostrarRegister() {
+    vista.mostrarPlantilla("register", "mainContenido")
 }
